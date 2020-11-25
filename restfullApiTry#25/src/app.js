a02aa302f80d521cf43abd51530892308a2c5cdd
Munsif <munsifalimisri69@gmail.com>
2020-11-25 21:36:36 +0500

    main execuatable file app.js

--- restfullApiTry#25/src/app.js
+++ restfullApiTry#25/src/app.js
@@ -3,7 +3,7 @@ require("./dbConnection/conn");//getting the connection
 const Student = require("./studemtsModel/models");//getting the student Model
 const port = process.env.PORT || 8000;
 const app = express();
-const Srouter = require("./Routers/routers");
+const Srouter = require("./Routers/routers");//getting all the router defined in routers file 
 //middle ware
 /*
     express.json() is use to recognize incoming requeest object as a JSON object 
@@ -16,3 +16,8 @@ app.use(Srouter);//to use router we have to tell express file that we are using 
 app.listen(port, (req, res) => {
     console.log(`listing to the port ${port}`);
 });
+// to add router we have to follow just three steps
+// 1: tell express file that u r using router like app.use(route) 
+// 2: importe the router class from express => const router = new express.Router();//to use router 
+// 3: make a seprate file and define all the possible routes like =>route.get("",(req,res)=>{}),,route.post("",(req,res)=>{}),,
+// route.patch("",(req,res)=>{}) and then just export the routes defined bcoz of reusability if we dont export not other file will get the router so its mandatory to export routes...
