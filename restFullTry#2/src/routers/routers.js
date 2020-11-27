b2ee3925eecef021c46e9942d06ea9085180b28b
Munsif <munsifalimisri69@gmail.com>
2020-11-27 23:20:25 +0500

    all the app routing like post,get,patch delete defined here

--- restFullTry#2/src/routers/routers.js
+++ restFullTry#2/src/routers/routers.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const [Student, Teacher] = require("../schema_Models/schemas_models");
 const routes = express.Router();
+
+/* --- START Create student/teaacher --- */
 routes.post("/student", async (req, res) => {
     try {
         const newStudent = await new Student(req.body);
@@ -25,11 +27,14 @@ routes.post("/teacher", async (req, res) => {
         res.status(400).send(newTeacher);
     }
 });
+/* --- END Create student/teaacher --- */
+
+/* --- START Get all the data available inside the table for student/teaacher --- */
 routes.get("/student", async (req, res) => {
     try {
         const studentsData = await Student.find();
         res.status(200).send(studentsData);
-        console.log(`Data shown successfully..`);
+        console.log(`Student data shown successfully..`);
     } catch (error) {
         console.log(`Error wile getting the student data : ${error}`);
         res.status(400).send();
@@ -39,18 +44,21 @@ routes.get("/teacher", async (req, res) => {
     try {
         const teacherData = await Teacher.find();
         res.status(200).send(teacherData);
-        console.log(`Data shown successfully..`);
+        console.log(`Teacher Data shown successfully..`);
     } catch (error) {
         console.log(`Error wile getting the student data : ${error}`);
         res.status(400).send();
     }
 });
+/* --- END get all the data available inside the table for student/teaacher --- */
+
+/* --- START find by id --- */
 routes.get("/student/:id", async (req, res) => {
     try {
         const studentId = req.params.id;
         const studentsData = await Student.find({ _id: studentId });
         res.status(200).send(studentsData);
-        console.log(`Data shown successfully..`);
+        console.log(`Studetn data shown successfully..`);
     } catch (error) {
         console.log(`Error wile getting the student data : ${error}`);
         res.status(400).send("Student id is invalid plz...")
@@ -64,10 +72,63 @@ routes.get("/teacher/:id", async (req, res) => {
             res.status(500).send("Teacher id is invalid...")
         }
         res.status(200).send(teacherData);
-        console.log(`Data shown successfully..`);
+        console.log(`Teacher data shown successfully..`);
     } catch (error) {
         console.log(`Error wile getting the student data : ${error}`);
         res.status(400).send("Teacher id is invalid plz...")
+    }
+});
+/* --- END find by id --- */
+
+/* --- START Update student/teaacher --- */
+routes.patch("/student/:id", async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        const studentBodyData = req.body;
+        const studentUpdatedData = await Student.findByIdAndUpdate({ _id: studentId }, studentBodyData, { new: true, useFindAndModify: true });//works like this => { _id:"32424"},{name:"xyx"}
+        res.status(200).send(studentUpdatedData);
+        console.log("Student data Updated ...");
+    } catch (error) {
+        console.log(`Error wile Updating the student data : ${error}`);
+        res.status(400).send("Student id is invalid ...")
+    }
+});
+routes.patch("/teacher/:id", async (req, res) => {
+    try {
+        const teacherId = req.params.id;
+        const teacherBodyData = req.body;
+        const teacherUpdatedData = await Teacher.findByIdAndUpdate({ _id: teacherId }, teacherBodyData, { new: true, useFindAndModify: true });//works like this => { _id:"32424"},{name:"xyx"}
+        res.status(200).send(teacherUpdatedData);
+        console.log("Teacher data Updated ...");
+    } catch (error) {
+        console.log(`Error wile Updating the Teacher data : ${error}`);
+        res.status(400).send("Teacher id is invalid ...")
+    }
+});
+/* --- End Update student/teaacher --- */
+/* --- START Delete student/teaacher --- */
+routes.delete("/student/:id", async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        const studentBodyData = req.body;
+        const studentUpdatedData = await Student.findByIdAndDelete({ _id: studentId });
+        res.status(200).send(studentUpdatedData);
+        console.log("Student data deleted..");
+    } catch (error) {
+        console.log(`Error wile Updating the student data : ${error}`);
+        res.status(400).send("Student id is invalid ...");
+    }
+});
+routes.delete("/teacher/:id", async (req, res) => {
+    try {
+        const teacherId = req.params.id;
+        const teacherBodyData = req.body;
+        const teacherUpdatedData = await Teacher.findByIdAndUpdate({ _id: teacherId }, teacherBodyData, { new: true });//works like this => { _id:"32424"},{name:"xyx"}
+        res.status(200).send(teacherUpdatedData);
+        console.log("Teacher data deleted..");
+    } catch (error) {
+        console.log(`Error wile Updating the Teacher data : ${error}`);
+        res.status(400).send("Teacher id is invalid ...")
     }
 });
 module.exports = routes;
