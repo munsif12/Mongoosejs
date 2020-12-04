bc4364b6b4f4c8f62c7d036687e309e78d38b511
Munsif <munsifalimisri69@gmail.com>
2020-12-04 22:21:30 +0500

    complete login and registration =>main exe file

--- mernbackend_login_registration/src/app.js
+++ mernbackend_login_registration/src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const hbs = require("hbs");
 require("./connection/connection");
 const userRegistration = require("./schemaModels/schemaModels");
+const async = require("hbs/lib/async");
 const port = process.env.PORT || 8000;
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));//if the data is comming from u
 app.get("/", (req, res) => {
     res.render("index");
 });
+//user registration
 app.get("/register", (req, res) => {
     res.render("register");
 });
@@ -32,7 +34,8 @@ app.post("/register", async (req, res) => {
                 password: req.body.pwd
             });
             const userData = await newUser.save();
-            res.status(201).send(userData);
+            console.log(`User registration Data : ${userData}`);
+            res.status(201).redirect("/login");
         }
         else
             res.send("password is not matching..")
@@ -41,8 +44,24 @@ app.post("/register", async (req, res) => {
         console.log(`Error while registering the user : ${error}`);
     }
 });
+//user login
 app.get("/login", (req, res) => {
     res.render("login");
+});
+app.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.pwd;
+        const userData = await userRegistration.findOne({ email: email });
+        if (userData.password === password) {
+            res.status(201).redirect("/");
+        }
+        else
+            res.send("invalid username or password");
+    } catch (error) {
+        console.log(`Error while login : ${error}`);
+        res.status(400).send(error);
+    }
 });
 app.listen(port, () => {
     console.log(`listenting to the request at ${port}`);
