fda105a60d34bb21d650b4f1cc772dbd56562112
Munsif <munsifalimisri69@gmail.com>
2020-12-15 23:13:49 +0500

    welcome page cript

--- mernbackend_login_registration/public/js/welcomePage.js
+++ mernbackend_login_registration/public/js/welcomePage.js
@@ -2,5 +2,9 @@ window.onload = () => {
     const covid = document.querySelector(".covid19");
     covid.addEventListener("click", () => {
         window.location.href = "/covid19";
+    });
+    const foodNutrition = document.querySelector(".foodNutrition");
+    foodNutrition.addEventListener("click", () => {
+        window.location.href = "/foodNutrition";
     })
 }
