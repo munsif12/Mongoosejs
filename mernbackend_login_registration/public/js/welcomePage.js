5d854641ae4e740e0bcd5cba3d229ce1c32d353f
Munsif <munsifalimisri69@gmail.com>
2021-01-10 21:27:51 +0500

    added reirect links to dropdownmanu

--- mernbackend_login_registration/public/js/welcomePage.js
+++ mernbackend_login_registration/public/js/welcomePage.js
@@ -1,9 +1,31 @@
 window.onload = () => {
+    const body = document.querySelector("body");
     const name = document.getElementById("name");
     const email = document.getElementById("email");
     const contectInfo = document.getElementById("contectInfo");
     const message = document.getElementById("message");
     const cancel = document.getElementById("cancel");
+    const darkMode = document.getElementById("darkMode");
+    const contectUs = document.getElementById("contect");
+    darkMode.addEventListener("click", () => {
+        console.log(darkMode.innerText);
+        if (darkMode.innerText == "Dark Mode") {
+            darkMode.innerText = "Light Mode";
+            contectUs.classList.add("darkModeCont");
+        }
+        else {
+            darkMode.innerText = "Dark Mode";
+            contectUs.classList.remove("darkModeCont");
+        }
+        body.classList.toggle("darkMode");
+
+
+    });
+    // if (darkMode.checked) {
+    //     alert("true");
+    //     console.log(darkMode.value);
+    // }
+    // else alert("not");
     cancel.addEventListener("click", () => {
         name.value = "";
         email.value = "";
